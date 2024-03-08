2704858bdd83f8b251acc6b429037d91781bbb01
Glen-Yegon <yegonglen@gmail.com>
2024-03-08 03:07:14 +0300

    first commit

---
+++ glen.dart
@@ -0,0 +1,10 @@
+void main() {
+  var a = 10;
+  if (a > 10) {
+    print("Your number is greater than 10");
+  } else if (a == 10) {
+    print("Your number is equal to 10");
+  } else {
+    print("Your number is less than 10");
+  }
+}
